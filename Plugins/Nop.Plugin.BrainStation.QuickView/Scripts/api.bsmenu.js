0a9422ac79a233b74cdb421272c1835cb4549fc8
bobbyware@outlook.com <bobbyware@outlook.com>
2015-05-15 21:12:54 +1200

    quick view

---
+++ Plugins/Nop.Plugin.BrainStation.QuickView/Scripts/api.bsmenu.js
@@ -0,0 +1,39 @@
+﻿
+
+
+var quickViewApi = function() {
+};
+
+
+quickViewApi.prototype.viewProductDetails = function(options) {
+    var config = $.extend({
+        data: { },
+        success: function() {
+        },
+        error: function() {
+        }
+    }, options);
+
+    $.apiCall({
+        type: 'POST',
+        data: config.data,
+        url: '/product_details',
+        success: function(html) {
+
+            $("#quick-view-modal").html(html);
+
+            $(document).trigger("hide-ajax-loading");
+            $("#quick-view-loading-modal").modal("hide");
+            $("#quick-view-product-details-modal").modal("show");
+
+        }
+    });
+
+};
+
+
+
+
+
+
+var api = new quickViewApi();
